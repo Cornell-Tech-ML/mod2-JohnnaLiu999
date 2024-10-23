6f1122f9fa02f73c9fcc4ab11acaf21e56cbe64b
JohnnaLiu999 <johnnaliu0518@gmail.com>
2024-10-22 17:26:48 -0700

    Your commit message

--- tests/strategies.py
+++ tests/strategies.py
@@ -1,6 +1,7 @@
 from hypothesis import settings
 from hypothesis.strategies import floats, integers
 
+from minitorch.operators import is_close
 
 settings.register_profile("ci", deadline=None)
 settings.load_profile("ci")
@@ -10,12 +11,6 @@ small_ints = integers(min_value=1, max_value=3)
 small_floats = floats(min_value=-100, max_value=100, allow_nan=False)
 med_ints = integers(min_value=1, max_value=20)
 
-
-def is_close(x: float, y: float) -> bool:
-    """$f(x) = |x - y| < 1e-2$"""
-    # ASSIGN 0.1
-    return (x - y < 1e-2) and (y - x < 1e-2)
-    # END ASSIGN 0.1
 
 
 def assert_close(a: float, b: float) -> None:
